9107c8447a01977acaac2cd7435d357c0a586125
g1257 <gz1@ornl.gov>
2014-07-17 14:08:04 -0400

    cluster functions added

---
+++ src/Engine/ClusterFunctions.h
@@ -0,0 +1,22 @@
+#ifndef CLUSTERFUNCTIONS_H
+#define CLUSTERFUNCTIONS_H
+
+namespace OpenDca {
+
+class ClusterFunctions {
+
+public:
+
+	typedef double RealType;
+
+	RealType operator()(RealType mu) const
+	{
+		return 0.0;
+	}
+
+}; // ClusterFunctions
+
+} // namespace OpenDca
+
+#endif // CLUSTERFUNCTIONS_H
+
