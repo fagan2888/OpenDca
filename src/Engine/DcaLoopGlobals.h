6fa3bd568d1c8edf34978d50eb8ff5d81ceb937d
g1257 <gz1@ornl.gov>
2014-08-05 15:40:13 -0400

    moving license to gpl

--- src/Engine/DcaLoopGlobals.h
+++ src/Engine/DcaLoopGlobals.h
@@ -1,72 +1,15 @@
 /*
-Copyright (c) 2014, UT-Battelle, LLC
-All rights reserved
-
-[OpenDca, Version 1.0.0]
-[by G.A., Oak Ridge National Laboratory]
-
-UT Battelle Open Source Software License 11242008
-
-OPEN SOURCE LICENSE
-
-Subject to the conditions of this License, each
-contributor to this software hereby grants, free of
-charge, to any person obtaining a copy of this software
-and associated documentation files (the "Software"), a
-perpetual, worldwide, non-exclusive, no-charge,
-royalty-free, irrevocable copyright license to use, copy,
-modify, merge, publish, distribute, and/or sublicense
-copies of the Software.
-
-1. Redistributions of Software must retain the above
-copyright and license notices, this list of conditions,
-and the following disclaimer.  Changes or modifications
-to, or derivative works of, the Software should be noted
-with comments and the contributor and organization's
-name.
-
-2. Neither the names of UT-Battelle, LLC or the
-Department of Energy nor the names of the Software
-contributors may be used to endorse or promote products
-derived from this software without specific prior written
-permission of UT-Battelle.
-
-3. The software and the end-user documentation included
-with the redistribution, with or without modification,
-must include the following acknowledgment:
-
-"This product includes software produced by UT-Battelle,
-LLC under Contract No. DE-AC05-00OR22725  with the
-Department of Energy."
-
-*********************************************************
-DISCLAIMER
-
-THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
-CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-COPYRIGHT OWNER, CONTRIBUTORS, UNITED STATES GOVERNMENT,
-OR THE UNITED STATES DEPARTMENT OF ENERGY BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-DAMAGE.
-
-NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED
-STATES DEPARTMENT OF ENERGY, NOR THE COPYRIGHT OWNER, NOR
-ANY OF THEIR EMPLOYEES, REPRESENTS THAT THE USE OF ANY
-INFORMATION, DATA, APPARATUS, PRODUCT, OR PROCESS
-DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
-
-*********************************************************
-
+This file is part of OpenDca.
+OpenDca is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+OpenDca is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with OpenDca. If not, see <http://www.gnu.org/licenses/>.
 */
 
 /** \ingroup DMRG */
